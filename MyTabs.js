96866018a97d2ed37fbe1838e2b615d7b1e3f00c
DianaTN <alumno.247863@ies-azarquiel.es>
2024-02-14 11:54:59 +0100

    Añadido Icono Home

--- MyTabs.js
+++ MyTabs.js
@@ -2,13 +2,21 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './screens/HomeScreen';
 import SettingsScreen from './screens/SettingsScreen';
 import ScreenTwo from './screens/ScreenTwo';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 export default function MyTabs() {
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                    tabBarIcon: () => (
+                        <MaterialCommunityIcons name="home" size={24} color="black" />
+                    )
+                }} />
             <Tab.Screen name="Settings" component={SettingsScreen} />
             <Tab.Screen name="Other" component={ScreenTwo} />
         </Tab.Navigator>
