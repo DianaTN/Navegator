2fb05113f74e8d6f3d452889e51d0b8c41691bab
DianaTN <alumno.247863@ies-azarquiel.es>
2024-02-14 10:59:02 +0100

    Añadido cambio a SettingsScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-
-
 export default function HomeScreen() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
