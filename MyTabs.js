2df85116c3eb2349e89bd0ca426b5a5b85029df2
DianaTN <alumno.247863@ies-azarquiel.es>
2024-02-14 12:02:27 +0100

    Finalizado modelaje icono Home

--- MyTabs.js
+++ MyTabs.js
@@ -8,13 +8,19 @@ const Tab = createBottomTabNavigator();
 
 export default function MyTabs() {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+            screenOptions={{
+                // Esto es lo que heredará luego en el icono
+                tabBarActiveTintColor: 'black'
+            }}>
+
             <Tab.Screen
                 name="Home"
                 component={HomeScreen}
                 options={{
-                    tabBarIcon: () => (
-                        <MaterialCommunityIcons name="home" size={24} color="black" />
+                    tabBarIcon: ({ color, size }) => (
+                        // Hereda el tamaño y el color del padre; por defecto se pone azul
+                        <MaterialCommunityIcons name="home" size={size} color={color} />
                     )
                 }} />
             <Tab.Screen name="Settings" component={SettingsScreen} />
